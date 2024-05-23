b076bad470095f988d756445b9708934bc716559
loyerluo <loyer-yuan@qq.com>
2024-05-23 11:24:35 +0800

    resolve the conflict of rebase the branch of dtr

---
+++ tools/llama_preprocess_data.sh
@@ -0,0 +1,10 @@
+tokenizer_path=/data/wangzehua/model_space/Llama-2-13b-hf/tokenizer.model
+dataset_path=/data/luoyuan/dataset/oscar-en-10k
+
+python preprocess_data.py \
+--input $dataset_path/oscar-en-10k-original/oscar-en-10k.jsonl \
+--output-prefix $dataset_path/oscar-en-10k-meg-llama \
+--tokenizer-type Llama2Tokenizer \
+--tokenizer-model $tokenizer_path \
+--workers 16 \
+--append-eod
